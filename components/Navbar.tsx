f34ae11e7047120f8649340b88170f4a2909cfc7
Yoan <yoanribot@gmail.com>
2022-01-24 00:29:09 +0100

    Fix detail before deploy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,9 +16,7 @@ import { FiKey } from "react-icons/fi";
 const Navbar = () => (
   <Flex p="2" borderBottom="1px" borderColor="gray.100">
     <Box fontSize="3xl" color="blue.400" fontWeight="bold">
-      <Link href="/" paddingLeft="2">
-        Real State
-      </Link>
+      <Link href="/">Real State</Link>
     </Box>
     <Spacer />
     <Box>
